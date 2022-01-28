0897b3eb53ea0e3da07b41c41e3f2dd8486205c4
SSAFY <jodie9596@gmail.com>
2022-01-28 17:05:30 +0900

    220128

---
+++ 22-01/4week/class2+_10816.py
@@ -0,0 +1,28 @@
+# 10816. 숫자 카드 2
+# Level Silver4
+# Link : https://www.acmicpc.net/problem/10816
+
+import sys
+
+my_input = sys.stdin.readline
+
+N = int(my_input())d
+cards = list(map(int, my_input().split()))
+cards.sort()
+M = int(my_input())
+result_cards = list(map(int, my_input().split()))
+minV, maxV = min(result_cards), max(result_cards)
+
+new_cards = list()
+for card in cards:
+    if minV <= card <= maxV:
+        new_cards.append(card)
+
+for card in result_cards:
+    cnt = 0
+    for card2 in new_cards:
+        if card == card2:
+            cnt += 1
+        elif card < card2:
+            break
+    print(cnt, end=' ')
