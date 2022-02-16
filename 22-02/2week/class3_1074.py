88c5f81533054c9dab2fce2179bd0455a108b921
jodie9596@gmail.com <jodie9596@gmail.com>
2022-02-16 21:26:51 +0900

    220216

---
+++ 22-02/2week/class3_1074.py
@@ -0,0 +1,18 @@
+# 9095. Z
+# Level Silver 1
+# Link : https://www.acmicpc.net/problem/1074
+
+import sys
+
+
+def Z(y, x, length, k):
+    place = 0
+    new_length = length // 2
+    
+    return
+
+
+my_input = sys.stdin.readline
+
+N, r, c = map(int, my_input().split())
+Z(r, c, 2 ** N, N)
