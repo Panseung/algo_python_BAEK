ad83e8a38afb7394fceadd512beba0a565eea944
jodie9596@gmail.com <jodie9596@gmail.com>
2022-02-24 23:00:05 +0900

    220224

---
+++ 22-02/2week/class1++_2741.py
@@ -0,0 +1,11 @@
+# 2741. N찍기
+# Level Bronze3
+# Link : https://www.acmicpc.net/problem/2741
+
+import sys
+
+my_input = sys.stdin.readline
+
+N = int(my_input())
+for i in range(1, N + 1):
+    print(i)
