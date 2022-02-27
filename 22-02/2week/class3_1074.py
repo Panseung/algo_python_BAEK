ccb11c8e584f610899e14aef33bd3c4f72e2d01f
SSAFY <jodie9596@gmail.com>
2022-02-27 18:21:31 +0900

    220227

--- 22-02/2week/class3_1074.py
+++ 22-02/2week/class3_1074.py
@@ -4,15 +4,24 @@
 
 import sys
 
-
-def Z(y, x, length, k):
-    place = 0
-    new_length = length // 2
-    
-    return
-
-
 my_input = sys.stdin.readline
 
 N, r, c = map(int, my_input().split())
-Z(r, c, 2 ** N, N)
+
+result = 0
+while N != 0:
+    N -= 1
+    comV = 2 ** N
+    if r >= comV:
+        if c >= comV:
+            result += comV ** 2 * 3
+            c -= comV
+        else:
+            result += comV ** 2 * 2
+        r -= comV
+    else:
+        if c >= comV:
+            result += comV ** 2
+            c -= comV
+
+print(result)
