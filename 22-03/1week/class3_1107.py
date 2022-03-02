5bab6e9041aae7776b2a256e18fd5e9d4ce2ea43
SSAFY <jodie9596@gmail.com>
2022-03-02 15:40:45 +0900

    220302

--- 22-03/1week/class3_1107.py
+++ 22-03/1week/class3_1107.py
@@ -6,35 +6,18 @@ import sys
 
 my_input = sys.stdin.readline
 
-
-def solve(v, k):
-    if k == length:
-        lst.append(v)
-        return
-    else:
-        for i in range(10):
-            pluns_num = int(v[k]) + i
-            minus_num = int(v[k]) - i
-            stop = False
-            if pluns_num < 10 and pluns_num not in broken_btn:
-                solve(pluns_num * 10 ** (length - 1 - k), k + 1)
-                stop = True
-            if minus_num >= 0 and minus_num not in broken_btn:
-                solve((minus_num + 1) * 10 ** (length - 1 - k) - 1, k + 1)
-                stop = True
-            if stop:
-                break
-
-
-N = my_input().rstrip()
+N = int(my_input().rstrip())
 M = int(my_input())
-broken_btn = list()
-move_100 = abs(100 - int(N))
-length = len(N)
+result = abs(100 - int(N))
 if M:
     broken_btn = list(map(int, my_input().split()))
-    lst = list()
-    solve(N, 0)
-    print(lst)
 else:
-    print(min(move_100, length))
+    broken_btn = list()
+
+for num in range(1000001):
+    for n in str(num):
+        if int(n) in broken_btn:
+            break
+    else:
+        result = min(result, len(str(num)) + abs(num - N))
+print(result)
---
+++ 22-03/1week/test.py
@@ -0,0 +1,6 @@
+for x in range(4):
+    if x == 2:
+        print('loop out')
+        break
+else:
+    print('loop end')
